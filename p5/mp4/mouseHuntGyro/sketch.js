409cd106897f82dee5b55097c3110797ab1a6f84
Kim Brucker <kkbruck2@ilstu.edu>
2020-01-23 05:38:55 -0600

    landscape

--- p5/mp4/mouseHuntGyro/sketch.js
+++ p5/mp4/mouseHuntGyro/sketch.js
@@ -69,7 +69,7 @@ function preload() {
 function setup() {
   createCanvas(1080, 720);
   angleMode(DEGREES);
-  // deviceOrientation(LANDSCAPE);
+  deviceOrientation(LANDSCAPE);
   fontDiner = loadFont('assets/FontdinerSwanky-Regular.ttf');
   bkgMusic.play();
   // requestT() ;
